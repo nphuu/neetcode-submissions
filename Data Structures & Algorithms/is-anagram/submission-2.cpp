1466ccde18b35febcd3df2a06037b5ab0697c6b8
nphuu <huub2509832@student.ctu.edu.vn>
2026-05-13 19:30:59 +0700

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return 0;
+        int check[26] = {0};
+        for(char c:s) check[c - 'a']++;
+        for(char c:t) check[c - 't']--;
+        for(int x : check)
+        {   
+            if(x!=0) return false;
+        }
+        return true;
+    }
+};
